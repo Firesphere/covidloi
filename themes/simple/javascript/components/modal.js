fbd5cae234beed714d3edf8cd5b5a10a0267b267
Simon `Firesphere` <firesphere@casa-laguna.net>
2021-10-23 12:31:17 +1300

    Remove unneeded templates etc.

--- themes/simple/javascript/components/modal.js
+++ themes/simple/javascript/components/modal.js
@@ -1,29 +1,25 @@
-if (!document.getElementsByClassName('GenericPage').length) {
-    document.addEventListener('click', function (e) {
-        let target = e.target;
+document.addEventListener('click', function (e) {
+    let target = e.target;
 
-        if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') === 'modal') {
-            if (target.hasAttribute('data-target')) {
-                let m_ID = target.getAttribute('data-target');
-                let content = pagecontent[m_ID];
-                document.getElementById('modal-title').innerHTML = content.Title;
-                document.getElementById('modal-content').innerHTML = content.Content;
-                document.getElementById('modal-item').classList.add('open');
-                // document.getElementById()
-                e.preventDefault();
-            }
-        }
-
-        // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
-        if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') === 'modal') || target.classList.contains('modal')) {
-            let modal = document.querySelector('[class="modal open"]');
-            modal.classList.remove('open');
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') === 'modal') {
+        if (target.hasAttribute('data-target')) {
+            let m_ID = target.getAttribute('data-target');
+            let content = pagecontent[m_ID];
+            document.getElementById('modal-title').innerHTML = content.Title;
+            document.getElementById('modal-content').innerHTML = content.Content;
+            document.getElementById('modal-item').classList.add('open');
+            // document.getElementById()
             e.preventDefault();
         }
-    }, false);
-}
+    }
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') === 'modal') || target.classList.contains('modal')) {
+        let modal = document.querySelector('[class="modal open"]');
+        modal.classList.remove('open');
+        e.preventDefault();
+    }
+}, false);
 if (!window.location.hash) {
-    document.getElementById('modal-title').innerHTML = pagecontent['menu1'].Title;
-    document.getElementById('modal-content').innerHTML = pagecontent['menu1'].Content;
     document.getElementById('modal-item').classList.add('open');
 }
--- themes/simple/templates/Layout/Page.ss
+++ themes/simple/templates/Layout/Page.ss
@@ -1,8 +1,8 @@
 <div id="modal-item" class="modal">
     <div class="modal-window large">
         <span class="close" data-dismiss="modal">&times;</span>
-        <h2 id="modal-title"></h2>
+        <h2 id="modal-title">$Title</h2>
 
-        <div id="modal-content"></div>
+        <div id="modal-content">$Content</div>
     </div>
 </div>
